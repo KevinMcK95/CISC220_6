ff87306931bd27efd5e585cfabc798111ed1bd69
KevinMcK95 <KevinMcK95@shaw.ca>
2016-12-07 18:37:02 -0500

    new mover function which returns string with proper lane position.

--- racegame.c
+++ racegame.c
@@ -17,15 +17,33 @@ void printer(char* str1, char* str2, char* str3, char* str4, char* str5){
 	return;
 }
 
+char* mover(char* str, int position, int moves, int len, int lane){
+	char newString[len], ending[10];
+	int i;
+	for (i=0; i<(position+moves); i++){
+		strcat(newString,"~");
+	}
+	strcat(newString,"|->");
+	for (i=(position+moves+3); i<(len-10); i++){
+		strcat(newString," ");
+	}
+	sprintf(ending,"# Lane %d #",lane);
+	strcat(newString,ending);
+	strcpy(str,newString);
+	return str;
+}
 
 int main(void){
 	int len = 40+3+10;
 	char str1[len], str2[len], str3[len], str4[len], str5[len];
 	strcpy(str1,"|->                                        # Lane 1 #");
-	strcpy(str2,"|->                                        # Lane 1 #");
-	strcpy(str3,"|->                                        # Lane 1 #");
-	strcpy(str4,"|->                                        # Lane 1 #");
-	strcpy(str5,"|->                                        # Lane 1 #");
+	strcpy(str2,"|->                                        # Lane 2 #");
+	strcpy(str3,"|->                                        # Lane 3 #");
+	strcpy(str4,"|->                                        # Lane 4 #");
+	strcpy(str5,"|->                                        # Lane 5 #");
 	printer(str1,str2,str3,str4,str5);
+	mover(str1,2,0,len,1);
+	printer(str1,str2,str3,str4,str5);
+
 	return 0;
 }
